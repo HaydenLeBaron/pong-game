a2650f43bc30c5a5db0fff359f1cac9238c5bc70
HaydenTheBaron <hlebaron98@gmail.com>
2020-04-29 00:57:31 -0600

    refactor all code to be object-oriented

--- pong.py
+++ pong.py
@@ -1,23 +1,26 @@
 # Pong game
 #
-# Based off of code from https://www.youtube.com/watch?v=Qf3-aDXG8q4
+# Originally based off of code from https://www.youtube.com/watch?v=Qf3-aDXG8q4
 
+
+# TODO: doc everything
+#
+# IDEAS
+# * Score
+# * PVP (wasd vs arrows)
+# * 2 player co-op
 
 import pygame, sys, random
 
 
 # Constants
 # -----------------------------------
-BALL_LENGTH = 30
-PADDLE_WIDTH = 10
-PADDLE_HEIGHT = 140
-BG_COLOR = pygame.Color('grey40')
-LIGHT_GREY = (10, 10, 10)
+
+BG_COLOR = pygame.Color('black')
+MIDLINE_COLOR_TUPLE = (255, 255, 255)
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 960
 FRAME_RATE = 60
-
-
 
 
 class Ball:
@@ -28,9 +31,9 @@ class Ball:
 
         self.diameter = diameter
         self.color_tuple = color_tuple
-        self.rect = pygame.Rect(SCREEN_WIDTH/2 - self.diameter/2,
-                                SCREEN_HEIGHT/2 - self.diameter/2,
-                                BALL_LENGTH, BALL_LENGTH)
+        self.rect = pygame.Rect(SCREEN_WIDTH/2 - diameter/2,
+                                SCREEN_HEIGHT/2 - diameter/2,
+                                diameter, diameter)
         self.x_speed = x_speed
         self.y_speed = y_speed
 
@@ -52,7 +55,7 @@ class Ball:
             self.restart()
 
         # Bounce balls on paddle collision
-        if self.rect.colliderect(player) or self.rect.colliderect(opponent):
+        if self.rect.colliderect(player) or self.rect.colliderect(bot):
             self.x_speed *= -1
 
     def restart(self):
@@ -63,43 +66,73 @@ class Ball:
 
 
 class Paddle:
-    pass
+    def __init__(self, color_tuple, side):
+        # side -- a string (either 'right' or 'left'), indicating what side the paddle is on (default: right)
+        
+        global SCREEN_HEIGHT, SCREEN_WIDTH
 
-class PlayerPaddle:
-    pass
+        self.width = 20
+        self.height = 150
+        self.color_tuple = color_tuple
+        self.y_speed = 0
 
-class AiPaddle:
-    pass
+        if side == 'right':
+            self.rect = pygame.Rect(SCREEN_WIDTH - 20,
+                                    SCREEN_HEIGHT/2 - self.height/2,
+                                    self.width, self.height)
+        else:
+            self.rect = pygame.Rect(10,
+                                    SCREEN_HEIGHT/2 - self.height/2,
+                                    self.width, self.height)
+
+
+
+class PlayerPaddle(Paddle):
+
+    def __init__(self, color_tuple, side):
+        super().__init__(color_tuple, side)
+
+    def move_to_next_frame(self):
+
+        global SCREEN_HEIGHT
+        player.rect.y += player.y_speed  # Move player
+
+        # Don't let player move off screen
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
+
+class AIPaddle(Paddle):
+
+    def __init__(self, color_tuple, side, target_ball):
+        super().__init__(color_tuple, side)
+        self.y_speed = 7
+        self.target_ball = target_ball
+
+    def move_to_next_frame(self):
+
+        global SCREEN_HEIGHT
+       
+        # Move bot
+        if self.rect.top < self.target_ball.rect.y:
+            self.rect.top += self.y_speed
+        if self.rect.bottom > self.target_ball.rect.y:
+            self.rect.bottom -= self.y_speed
+
+        # Don't let bot move off screen
+        if self.rect.top <= 0:
+            self.rect.top = 0
+        if self.rect.bottom >= SCREEN_HEIGHT:
+            self.rect.bottom = SCREEN_HEIGHT
+
 
 
 # Drawing helpers
 # -----------------------------------
 
 
-def player_animation():
-
-    player.y += player_speed  # Move player
-
-    # Don't let player move off screen
-    if player.top <= 0:
-        player.top = 0
-    if player.bottom >= SCREEN_HEIGHT:
-        player.bottom = SCREEN_HEIGHT
-
-
-def opponent_ai():
-
-    # Move opponent
-    if opponent.top < ball.rect.y:
-        opponent.top += opponent_speed
-    if opponent.bottom > ball.rect.y:
-        opponent.bottom -= opponent_speed
-
-    # Don't let opponent move off screen
-    if opponent.top <= 0:
-        opponent.top = 0
-    if opponent.bottom >= SCREEN_HEIGHT:
-        opponent.bottom = SCREEN_HEIGHT
 
 
 
@@ -117,7 +150,7 @@ screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption('Pong')
 
 
-# Game Rectangles
+# Game Objects
 # -----------------------------------
 
 # NOTE: origin (0, 0) is in top left corner
@@ -126,15 +159,8 @@ ball = Ball(30, pygame.Color('yellow'),
             7 * random.choice((1, -1)),
             7 * random.choice((1, -1)))
 
-player = pygame.Rect(SCREEN_WIDTH - 20,
-                   SCREEN_HEIGHT/2 - PADDLE_HEIGHT/2,
-                     PADDLE_WIDTH, PADDLE_HEIGHT)
-opponent = pygame.Rect(10,
-                   SCREEN_HEIGHT/2 - PADDLE_HEIGHT/2,
-                     PADDLE_WIDTH, PADDLE_HEIGHT)
-
-player_speed = 0
-opponent_speed = 7
+player = PlayerPaddle((0, 0, 240), 'right')
+bot = AIPaddle((240, 0, 0), 'left', ball)
 
 
 
@@ -149,26 +175,26 @@ while True:
             sys.exit()
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_DOWN:
-                player_speed += 7
+                player.y_speed += 7
             if event.key == pygame.K_UP:
-                player_speed -= 7
+                player.y_speed -= 7
         if event.type == pygame.KEYUP:
             if event.key == pygame.K_DOWN:
-                player_speed -= 7
+                player.y_speed -= 7
             if event.key == pygame.K_UP:
-                player_speed += 7
+                player.y_speed += 7
 
 
     # Shift game object locations
     ball.move_to_next_frame()
-    player_animation()
-    opponent_ai()
+    player.move_to_next_frame()
+    bot.move_to_next_frame()
 
     # Draw game objects
     screen.fill(BG_COLOR)
-    pygame.draw.rect(screen, LIGHT_GREY, player)
-    pygame.draw.rect(screen, LIGHT_GREY, opponent)
-    pygame.draw.aaline(screen, LIGHT_GREY,
+    pygame.draw.rect(screen, player.color_tuple, player)
+    pygame.draw.rect(screen, bot.color_tuple, bot)
+    pygame.draw.aaline(screen, MIDLINE_COLOR_TUPLE,
                        (SCREEN_WIDTH/2, 0),
                        (SCREEN_WIDTH/2, SCREEN_HEIGHT))
     pygame.draw.ellipse(screen, ball.color_tuple, ball.rect)
